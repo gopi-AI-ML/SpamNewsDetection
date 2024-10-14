0b02e4e5f12266cb27bb443492bda0b1fd89435d
Gopi Krishna Ch <gopiaiml@gmail.com>
2024-10-14 16:19:54 +0530

    modified src/logger.py

--- src/logger.py
+++ src/logger.py
@@ -1,9 +1,10 @@
 import logging
+import logging
 import os
 
 # Define the logs folder and log file name
 logs_folder = "logs"
-LOG_FILE = "info.log"  # Ensure the log file name is a string with a .log extension
+LOG_FILE = "info.log" 
 
 # Create the full path for the log file
 LOG_FILE_PATH = os.path.join(logs_folder, LOG_FILE)
@@ -12,16 +13,15 @@ LOG_FILE_PATH = os.path.join(logs_folder, LOG_FILE)
 os.makedirs(logs_folder, exist_ok=True)
 
 # Set up the logging configuration
-logging.basicConfig(
+def setup_logging():
+    logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="%(asctime)s - %(message)s",
     level=logging.INFO,
     filemode="w"
 )
+    
+setup_logging()
 
-if __name__ == "__main__":
-    # Log messages to test
-    logging.info("bye bye ...")
-    logging.info("This is an info message.")
-    logging.warning("This is a warning message.")
-    logging.error("This is an error message.")
+if __name__=="__main__":
+    logging.info("bye bye...")
